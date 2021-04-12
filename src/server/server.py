5e008166e45735726601856f606c5ef9a9bfe150
Rocio Cruz Linares <rocio.x.linares95@gmail.com>
2021-04-11 22:12:28 -0400

    feat: Create endpoints to retrieve the best players of the week and the season

--- src/server/server.py
+++ src/server/server.py
@@ -1,26 +1,87 @@
 import json
 
 from flask import request
+from sqlalchemy.sql import text
 
 from . import create_app
 from .database import repository
 from .database.models import *
 
 app = create_app()
+engine = db.get_engine(app=app)
 
 ENTITY_MAPPER = {
     "players": Player,
     "teams": Team,
     "games": Game,
-    "gamedetails": GameDetail,
+    "games_details": GameDetail,
     "leagues": League,
     "rankings": Ranking,
+    "players_teams": PlayersTeams,
 }
 
 
 @app.route("/", methods=["GET"])
 def index():
     return json.dumps({"entities": list(ENTITY_MAPPER.keys())}), 200
+
+
+@app.route("/players/week/<date>", methods=["GET"])
+def week_best_players(date: str):
+    data = request.get_json()
+    limit = 1
+    if data:
+        limit = data.get("limit", 1)
+
+    with engine.connect() as con:
+        statement = text(
+            """
+            SELECT game_detail.player_name,game_detail.reb ,game_detail.ast ,  game_detail.pts ,game_detail.reb + game_detail.ast  +  game_detail.pts as total_stats, game.game_date_est 
+            FROM game_detail, game
+            WHERE
+                game_detail.game_id = game.id
+                AND
+                YEARWEEK(:date) = YEARWEEK(game.game_date_est)
+            ORDER BY total_stats DESC
+            LIMIT  :limit;
+            """
+        )
+
+        rs = con.execute(statement, **{"date": date, "limit": limit})
+
+    return json.dumps([{k: str(v) for k, v in dict(r).items()} for r in rs]), 200
+
+
+@app.route("/players/season/<year>", methods=["GET"])
+def season_best_player(year: str):
+    data = request.get_json()
+    limit = 1
+    if data:
+        limit = data.get("limit", 1)
+
+    result = []
+    with engine.connect() as con:
+        for week in range(1, 54):
+
+            statement = text(
+                """
+                SELECT game_detail.player_name,game_detail.reb ,game_detail.ast ,  game_detail.pts ,game_detail.reb + game_detail.ast  +  game_detail.pts as total_stats, game.game_date_est 
+                FROM game_detail, game
+                WHERE
+                    game_detail.game_id = game.id
+                    AND
+                    :yearweek = YEARWEEK(game.game_date_est)
+                ORDER BY total_stats DESC
+                LIMIT  :limit;
+                """
+            )
+
+            rs = con.execute(statement, **{"yearweek": f"{year}{week}", "limit": limit})
+            players = [{k: str(v) for k, v in dict(r).items()} for r in rs]
+            if players:
+                result.append({"week": week, "best_players": players})
+
+    return json.dumps(result), 200
 
 
 @app.route("/<entity>", methods=["GET"])
@@ -50,3 +111,4 @@ def edit(entity, record_id):
     data = request.get_json()
     repository.edit(ENTITY_MAPPER[entity], id=record_id, **data)
     return json.dumps("Edited"), 200
+
